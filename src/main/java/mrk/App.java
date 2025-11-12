1e070225785854ec27074bc448cd92c5d024ef79
mrk <s.max2003@yandex.ru>
2025-11-12 20:53:26 +0300

    new test4: update documentation generation

--- src/main/java/mrk/App.java
+++ src/main/java/mrk/App.java
@@ -2,10 +2,12 @@ package mrk;
 
 // Test comment
 // Test comment
+// Test comment
 public class App {
     public static void main(String[] args) {
         System.out.println(MathUtils.add(2, 3));
         System.out.println(MathUtils.square(2));
+        System.out.println(MathUtils.factorial(2));
         System.out.println(MathUtils.factorial(2));
     }
 }
