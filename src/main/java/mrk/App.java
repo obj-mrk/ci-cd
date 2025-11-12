e57006850f41e042ef8ce1e206840d2777a920d4
mrk <s.max2003@yandex.ru>
2025-11-12 20:18:11 +0300

    add method factorial

--- src/main/java/mrk/App.java
+++ src/main/java/mrk/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MathUtils.add(2, 3));
         System.out.println(MathUtils.square(2));
+        System.out.println(MathUtils.factorial(2));
     }
 }
