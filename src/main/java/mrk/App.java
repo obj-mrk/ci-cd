a7fa7cf269d430e50af799316b89075e0cf8bee5
mrk <s.max2003@yandex.ru>
2025-11-12 19:43:43 +0300

    first commit

---
+++ src/main/java/mrk/App.java
@@ -0,0 +1,7 @@
+package mrk;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(MathUtils.add(2, 3));
+    }
+}
---
+++ src/main/java/mrk/MathUtils.java
@@ -0,0 +1,7 @@
+package mrk;
+
+public class MathUtils {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
